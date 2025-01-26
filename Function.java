035598f62feb9dcffb5b796bfc16147225fbaee7
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-01-26 22:55:23 +0530

    functions

---
+++ Function.java
@@ -0,0 +1,28 @@
+// public class Function {
+//      public static void main(String[] args) {
+//         int ans = sum2 ();
+//         System.out.println(ans);
+//      }
+
+//      static int sum2() {
+//         int a = 10;
+//         int b = 20;
+//         return a + b;
+//      }
+// }
+
+// returning a string
+public class Function{
+
+   public static void main(String[] args) {
+      String ans = alpha ();
+
+      System.out.println(ans);
+   
+   }
+
+   static String alpha() {
+      return "hello";
+   }
+
+}
