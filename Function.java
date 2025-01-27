077efa9a1269daca8ae610e7a8933e77e7008d9d
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-01-27 23:47:32 +0530

    params

--- Function.java
+++ Function.java
@@ -11,18 +11,48 @@
 //      }
 // }
 
-// returning a string
+// ................................................................returning a string
+// public class Function{
+
+//    public static void main(String[] args) {
+//       String ans = alpha ();
+
+//       System.out.println(ans);
+   
+//    }
+
+//    static String alpha() {
+//       return "hello";
+//    }
+
+// }
+
+
+//..................................................................arguments
+
+
+//pass the value of nummbers when u are clling the method in main()
+
+
 public class Function{
 
    public static void main(String[] args) {
-      String ans = alpha ();
+      // System.out.println(sum(10, 20));
 
-      System.out.println(ans);
-   
-   }
+      
+   } 
 
-   static String alpha() {
-      return "hello";
-   }
 
+   // static int sum(int a, int b) {
+   //    return a + b;
+      
+   // }
+
+      static void swap(int a, int b) {
+         int temp = a;
+         a = b;   
+         b = temp;
+
+
+      }
 }
