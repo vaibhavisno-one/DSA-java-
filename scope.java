32b500ee5ddb9a3ba2657b8970c17fe99a6b38d7
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-01-31 00:11:59 +0530

    scope

---
+++ scope.java
@@ -0,0 +1,25 @@
+public class scope {
+
+    // 
+    
+
+    // block scope
+
+    // public static void main(String[] args) {
+
+    //     int a = 10;
+    //     int b= 20;
+        
+    //     {
+    //         // int a = 100;  block scope == ucan modify it but can nnot intialize it
+    //         int d = 30;//value intialized in this block will remain in this block
+        
+    //     }
+    //     // System.out.println(d);
+    // }
+
+
+
+    
+    
+}
